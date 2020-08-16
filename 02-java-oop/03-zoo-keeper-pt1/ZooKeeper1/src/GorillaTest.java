e814d550a7469098042a2c31d1cfb121cb4675d3
Blair Bryant <blairslc@gmail.com>
2020-08-15 18:43:50 -0600

    Finished ZooKeeper1

---
+++ 02-java-oop/03-zoo-keeper-pt1/ZooKeeper1/src/GorillaTest.java
@@ -0,0 +1,16 @@
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gorilla test = new Gorilla();
+		test.throwSomething();
+		test.throwSomething();
+		test.throwSomething();
+		test.eatBananas();
+		test.eatBananas();
+		test.climb();
+		test.displayEnergy();
+	}
+
+}
