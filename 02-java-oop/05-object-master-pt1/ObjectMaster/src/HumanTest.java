7dd1281fa0f660b761e00fa1322e54e4a646b600
Blair Bryant <blairslc@gmail.com>
2020-08-15 19:02:49 -0600

    Finished ObjectMaster1

---
+++ 02-java-oop/05-object-master-pt1/ObjectMaster/src/HumanTest.java
@@ -0,0 +1,12 @@
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human human = new Human();
+		Human human2 = new Human();
+		human.attack(human2);
+		human2.getHealth();
+	}
+
+}
