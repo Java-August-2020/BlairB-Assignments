cbbd6bfe97137d3ec28c9ba51dcd3bad0dc414ca
Blair Bryant <blairslc@gmail.com>
2020-08-20 19:51:33 -0600

    Finished Strings

---
+++ 04-java-spring/01-spring-fundamentals/01-strings/Strings/src/main/java/com/blair/strings/StringsApplication.java
@@ -0,0 +1,28 @@
+package com.blair.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	
+	public String hello() {
+		return "Hello client! How's it goin?";
+	}
+	
+	@RequestMapping("random")
+	
+	public String random() {
+		return "Spring Boot is great!";
+	}
+}
