f0603e341352b8da4811bb8f55e031d51ab69532
Blair Bryant <blairslc@gmail.com>
2020-08-24 14:34:41 -0600

    Finished Counter

---
+++ 04-java-spring/01-spring-fundamentals/05-counter/Counter/src/main/java/com/blair/counter/controllers/CounterController.java
@@ -0,0 +1,33 @@
+package com.blair.counter.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CounterController {
+	@RequestMapping("/")
+	public String index(HttpSession session) {
+		if (session.getAttribute("count") == null) {
+			session.setAttribute("count", 0);
+		} else {
+			Integer currentCount = (Integer) session.getAttribute("count");
+			currentCount++;
+			session.setAttribute("count", currentCount);
+		}
+		System.out.println(session.getAttribute("count"));
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/counter")
+	public String counter(HttpSession session, Model model) {
+		if (session.getAttribute("count") == null) {
+			model.addAttribute("counter", 0);
+		} else {
+			model.addAttribute("counter", session.getAttribute("count"));
+		}
+		return "counter.jsp";
+	}
+}
---
+++ 04-java-spring/01-spring-fundamentals/05-counter/Counter/src/main/webapp/WEB-INF/counter.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+</head>
+<body>
+	<p>You have visited <a href="/">http://localhost:8080</a> ${counter} times.</p>
+	<a href="/">Test another visit?</a>
+</body>
+</html>
